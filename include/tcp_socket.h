de76208a23282c30ef081fc122adfff3e10a8e94
Zack Kollar <rallokkcaz@gmail.com>
2017-09-26 00:02:39 -0700

    Added comments to source files.

--- include/tcp_socket.h
+++ include/tcp_socket.h
@@ -7,6 +7,7 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+// Struct to store a TCP/IP socket and it's related values. 
 typedef struct _tcp_socket {
     int file_descriptor;
     int port_number;
@@ -16,18 +17,24 @@ typedef struct _tcp_socket {
     int child_process_count;
 } tcp_socket;
 
+// Initialize a tcp_socket struct.
 int tcp_socket_create(
     tcp_socket* sock,
     int port_number,
     int buffer_size
 );
+// Listen on the specified port and run specified handler function.
 int tcp_socket_listen(
     tcp_socket* sock,
     void (*handler)(tcp_socket*, int)
 );
+// Handle keyboard interrupts.
 void tcp_socket_handle_interrupt(int signal, int connection_file_desc);
+// Destroy a tcp_socket struct from memory.
 void tcp_socket_destroy(tcp_socket* sock);
 
+
+// Macro to shorten writing a common function.
 #define tcp_socket_write(sock, connection_file_desc, format, ...)  \
     snprintf(sock->buffer, sock->buffer_size, (format), ##__VA_ARGS__); \
     write(connection_file_desc, sock->buffer, strlen(sock->buffer));
--- src/tcp_socket.c
+++ src/tcp_socket.c
@@ -6,29 +6,37 @@ int tcp_socket_create (
     const int buffer_size
 )
 {
+    // Create a INET TCP socket fd and store it on our struct.
     sock->file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
     
+    // Check everything went okay.
     if(sock->file_descriptor < 0)
         perror("Error opening socket...");
 
+    // Set our socket to have the SO_REUSEADDR option set.
     if (setsockopt(sock->file_descriptor, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int)) < 0)
         perror("setsockopt(SO_REUSEADDR) failed");
     
+    // Allocate space for the socket address struct.
     memset(&sock->socket, 0, sizeof(sock->socket));
 
+    // Allocate the sockets buffer dynamically.
     sock->buffer_size = buffer_size;
     size_t __buffer_size = buffer_size * sizeof(char);
     sock->buffer = malloc(__buffer_size);
     memset(sock->buffer, '\0', __buffer_size);
 
+    // Set values on our socket address struct.
     sock->socket.sin_family = AF_INET;
     sock->socket.sin_addr.s_addr = htonl(INADDR_ANY);
     sock->socket.sin_port = htons(port_number);
    
+    // Return everything went okay!
     return 0;
 }
 
 void tcp_socket_interrupt(const int signal) {
+    // Handle interrupt.
     puts("\nServer shutting down due to intterupt...");
     exit(0);
 }
@@ -38,9 +46,11 @@ int tcp_socket_listen(
     void (*handler)(tcp_socket*, int)
 )
 {
+    // Bind on specified port.
     bind(sock->file_descriptor, (struct sockaddr*)&sock->socket, sizeof(sock->socket));
     listen(sock->file_descriptor, SOMAXCONN);
 
+    // Set intterup signal.
     signal(SIGINT, tcp_socket_interrupt);
 
     while(1)
@@ -53,7 +63,8 @@ int tcp_socket_listen(
         pid_t pid;
         if((pid = fork()) < 0) // If the fork fails...
             perror("Failed to fork new process.");
-        else if(pid == 0) { // If it's a child process close and handle.
+        else if(pid == 0) { // If it's a child process
+                            // close server socket and handle.
             close(sock->file_descriptor);
             handler(sock, connection_file_desc);
 
@@ -65,7 +76,7 @@ int tcp_socket_listen(
         close(connection_file_desc);
         sock->child_process_count++;
 
-        // Clean up.
+        // Clean up child processes.
         while(sock->child_process_count) {
             pid = waitpid((pid_t) -1, NULL, WNOHANG);
             if (pid == 0)
@@ -80,6 +91,7 @@ int tcp_socket_listen(
 }
 
 void tcp_socket_destroy(tcp_socket* sock) {
+    // Deallocate our used memory.
     free(&sock->buffer);
     free(&sock->socket);
     close(sock->file_descriptor);
